390821553783a2284e0018bc52f9e2fde2a701cf
DebjitPramanick <dpramanick9990@gmail.com>
2022-07-25 23:11:01 +0530

    Feat: Created todo structure

--- server/main.go
+++ server/main.go
@@ -7,6 +7,13 @@ import (
 	"github.com/gofiber/fiber/v2"
 )
 
+type Todo struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+	Done  bool   `json:"done"`
+	Body  string `json:"body"`
+}
+
 func main() {
 	fmt.Println("Server is running...")
 	app := fiber.New()
@@ -14,7 +21,6 @@ func main() {
 	app.Get("/healthz", func(c *fiber.Ctx) error {
 		return c.SendString("APP IS RUNNING")
 	})
-
 
 	log.Fatal(app.Listen(":4000"))
 }
